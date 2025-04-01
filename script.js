9186aab231ee295f8efd18bde86a61290a849fce
alirezamohammadpoor <alirezamohammadp@gmail.com>
2025-04-01 19:50:35 +0200

    "Add operate function with basic math operations
    - Implement add, subtract, multiply, divide functions
    - Add operate function to handle operator selection
    - Include error handling for division by zero"

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 function add(num, num2) {
-  return num + num2
+  return num + num2;
 }
 
 function subtract(num, num2) {
-  return num - num2
+  return num - num2;
 }
 
 function multiply(num, num2) {
-  return num * num2
+  return num * num2;
 }
 
 function divide(num, num2) {
   if (num2 === 0) {
-    return "ERROR"
+    return "ERROR";
   } else {
-    return num / num2
+    return num / num2;
   }
 }
 
-function operate()
+function operate(num1, operator, num2) {
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  }
+}
